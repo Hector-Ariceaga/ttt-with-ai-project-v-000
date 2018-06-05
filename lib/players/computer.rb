2df7a92ddc13b6d0b44036117b7d32226f97e163
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-06-05 22:06:22 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -14,9 +14,35 @@ module Players
           '7'
         elsif !board.taken?(1)
           '1'
+        elsif !board.taken(3)
+          '3'
+        elsif !board.taken(9)
+          '9'
         end
       when 2 
-        if !board.taken()
+        if !board.taken(3)
+          '3'
+        elsif !board.taken(1)
+          '1'
+        elsif !board.taken(9)
+          '9'
+        end
+      when 3
+        if !board.taken(3)
+          '3'
+        elsif !board.taken(7)
+          '7'
+        elsif !board.taken(9)
+          '9'
+        end 
+      when 4
+        if board.taken(7) && board.taken(1)
+          '4'
+        elsif board.taken(7) && board.taken(3)
+          '2'
+        elsif board.taken(7) && board.taken(9)
+          '8'
+        end
         
         
       end
