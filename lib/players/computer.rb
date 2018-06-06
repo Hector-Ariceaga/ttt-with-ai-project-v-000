0ffef5b7ef048b4d724862992c7d4146cca6e073
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-06-06 00:31:27 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -235,25 +235,6 @@ module Players
         elsif !board.taken?(9)
          '9'
         end
-        elsif !board.taken?(1)
-          '1'
-        elsif !board.taken?(2)
-          '2'
-        elsif !board.taken?(3)
-          '3'
-        elsif !board.taken?(4)
-          '4'
-        elsif !board.taken?(5)
-          '5'
-        elsif !board.taken?(6)
-          '6'
-        elsif !board.taken?(7)
-          '7'
-        elsif !board.taken?(8)
-          '8'
-        elsif !board.taken?(9)
-          '9'
-        end
       when 7
         if board.position(1) == 'X' && board.position(2) == 'X'
           '3'
